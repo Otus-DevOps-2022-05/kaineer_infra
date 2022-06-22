c5d7d7e428aa7c27e84cfe95255619f587d99171
Sergey Kluchkovsky <kaineer@gmail.com>
2022-06-22 20:30:47 +0500

    Creates instance again, with another IP

--- startup.sh
+++ startup.sh
@@ -3,7 +3,6 @@
 yc compute instance create \
   --name reddit-app \
   --hostname reddit-app \
-  --preemptible \
   --memory=4 \
   --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1604-lts,size=10GB \
   --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
