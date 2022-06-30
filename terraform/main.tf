83e459d64491b5b2d655592bca2095c7b50fca06
Sergey Kluchkovsky <kaineer@gmail.com>
2022-06-30 09:43:53 +0000

    Add balancer, fix provisioning

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
-      version = ">= 0.35"
+      source = "yandexcloud/yandex"
+      version = "~> 0.35"
     }
   }
   required_version = ">= 0.13"
