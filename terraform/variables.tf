b60ec100cd3db4720dc2059bc20574a4f7cc58c7
Sergey Kluchkovsky <kaineer@gmail.com>
2022-06-30 05:07:58 +0000

    Add main.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable {
+    name = "token"
+    type = "string"
+}
+
+variable {
+    name = "cloud"
+    type = "string"
+}
+
+variable {
+    name = "folder_id"
+    type = "string"
+}
+
+variable {
+    name = "subnet_id"
+    type = "string"
+}
+
+variable {
+    name = "zone"
+    type = "string"
+}
