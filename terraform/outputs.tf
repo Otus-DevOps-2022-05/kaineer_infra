3662f03f0318dd340fe7d31168d11d318914a7f5
Sergey Kluchkovsky <kaineer@gmail.com>
2022-07-02 06:58:32 +0000

    Hides unavailabe vars

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,10 @@ output "external_ip_instance_1" {
   value = yandex_compute_instance.app[0].network_interface.0.nat_ip_address
 }
 
-output "external_ip_instance_2" {
-  value = yandex_compute_instance.app[1].network_interface.0.nat_ip_address
-}
-
-output "external_ip_lb" {
-  value = yandex_lb_network_load_balancer.reddit-app-lb.listener.*.external_address_spec[0].*.address[0]
-}
+// output "external_ip_instance_2" {
+//   value = yandex_compute_instance.app[1].network_interface.0.nat_ip_address
+// }
+// 
+// output "external_ip_lb" {
+//   value = yandex_lb_network_load_balancer.reddit-app-lb.listener.*.external_address_spec[0].*.address[0]
+// }
