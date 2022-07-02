6c91a6bd1858d4abe75ee1bb5b881123e63b0253
Sergey Kluchkovsky <kaineer@gmail.com>
2022-07-02 06:44:51 +0000

    Исправляет desc на description

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,41 +1,41 @@
 variable "token" {
   type = string
-  desc = "Токен для доступа к облаку"   
+  description = "Токен для доступа к облаку"   
   default = ""
 }
 
 variable "cloud" {
   type = string
-  desc = "Идентификатор облака"
+  description = "Идентификатор облака"
   default = ""
 }
 
 variable "folder_id" {
   type = string
-  desc = "Каталог для размещения ресурсов"
+  description = "Каталог для размещения ресурсов"
   default = ""
 }
 
 variable "subnet_id" {
   type = string
-  desc = "Идентификатор подсети"
+  description = "Идентификатор подсети"
   default = ""
 }
 
 variable "zone" {
   type = string
-  desc = "Зона, в которой размещаются подсети"
+  description = "Зона, в которой размещаются подсети"
   default = ""
 }
 
 variable "private_key_file" {
   type = string
-  desc = "Путь к файлу с приватным ключом"
+  description = "Путь к файлу с приватным ключом"
   default = ""
 }
 
 variable "reddit_app_image" {
   type = string
-  desc = "Образ с приложением"
+  description = "Образ с приложением"
   default = ""
 }
