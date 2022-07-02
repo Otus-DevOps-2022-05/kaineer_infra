dc5b725c47b2ca394526b55819ba7357c5cdb33e
Sergey Kluchkovsky <kaineer@gmail.com>
2022-07-02 06:38:10 +0000

    Add descriptions to variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,27 +1,41 @@
 variable "token" {
   type = string
+  desc = "Токен для доступа к облаку"   
+  default = ""
 }
 
 variable "cloud" {
   type = string
+  desc = "Идентификатор облака"
+  default = ""
 }
 
 variable "folder_id" {
   type = string
+  desc = "Каталог для размещения ресурсов"
+  default = ""
 }
 
 variable "subnet_id" {
   type = string
+  desc = "Идентификатор подсети"
+  default = ""
 }
 
 variable "zone" {
   type = string
+  desc = "Зона, в которой размещаются подсети"
+  default = ""
 }
 
 variable "private_key_file" {
   type = string
+  desc = "Путь к файлу с приватным ключом"
+  default = ""
 }
 
 variable "reddit_app_image" {
   type = string
+  desc = "Образ с приложением"
+  default = ""
 }
