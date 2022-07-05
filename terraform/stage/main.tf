d430b353406fab12dcb3b82441925ceb7269a3d1
Sergey Kluchkovsky <kaineer@gmail.com>
2022-07-05 07:09:37 +0000

    Link to providers.tf

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "~> 0.75"
-    }
-  }
-  required_version = ">= 0.13"
-}
-
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
   cloud_id  = var.cloud
