6a4f5846d0566cca82bc7ff15304c32177068b2d
Sergey Kluchkovsky <kaineer@gmail.com>
2022-07-08 14:59:54 +0300

    Hides providers. Again

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,8 +1,8 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  required_version = ">= 0.13"
-}
+// terraform {
+//   required_providers {
+//     yandex = {
+//       source = "yandex-cloud/yandex"
+//     }
+//   }
+//   required_version = ">= 0.13"
+// }
